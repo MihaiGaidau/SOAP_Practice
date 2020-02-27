e4f0e347e47ba91c1b0afff296c751dacdc61c8e
MihaiGaidau <gaidaumihai@gmai.com>
2020-02-27 18:12:45 +0200

    done with get an element by id

--- firstExample/src/main/java/com/learnSOAP/practice/firstExample/configurations/WebServiceConfig.java
+++ firstExample/src/main/java/com/learnSOAP/practice/firstExample/configurations/WebServiceConfig.java
@@ -1,5 +1,6 @@
 package com.learnSOAP.practice.firstExample.configurations;
 
+import com.learnSOAP.practice.firstExample.service.CourseDetailsService;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
--- firstExample/src/main/java/com/learnSOAP/practice/firstExample/soap/CourseDetailsEndpoint.java
+++ firstExample/src/main/java/com/learnSOAP/practice/firstExample/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,7 @@
 package com.learnSOAP.practice.firstExample.soap;
 
+import com.learnSOAP.practice.firstExample.bean.Course;
+import com.learnSOAP.practice.firstExample.service.CourseDetailsService;
 import com.mgaidau.soappractice.courses.CourseDetails;
 import com.mgaidau.soappractice.courses.GetCourseDetailsRequest;
 import com.mgaidau.soappractice.courses.GetCourseDetailsResponse;
@@ -10,16 +12,32 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class CourseDetailsEndpoint {
+    private final CourseDetailsService courseDetailsService;
+
+    public CourseDetailsEndpoint(CourseDetailsService courseDetailsService) {
+        this.courseDetailsService = courseDetailsService;
+    }
 
     @PayloadRoot(localPart = "GetCourseDetailsRequest", namespace = "http://mgaidau.com/SOAPpractice/courses")
     @ResponsePayload
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request){
-       GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+
+        Course course = courseDetailsService.findById(request.getId());
+
+        return mapCourseDetails(course);
+    }
+
+    private CourseDetails mapCourse(Course course) {
         CourseDetails courseDetails = new CourseDetails();
-        courseDetails.setId(request.getId());
-        courseDetails.setName("Microservices Course");
-        courseDetails.setDescription("good course");
-       return response;
+        courseDetails.setId(course.getId());
+        courseDetails.setName(course.getName());
+        courseDetails.setDescription(course.getDescription());
+        return courseDetails;
+    }
+    private GetCourseDetailsResponse mapCourseDetails(Course course) {
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+        response.setCourseDetails(mapCourse(course));
+        return response;
     }
 
 
