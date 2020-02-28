85726bc705462ef8a0b2eb257187621f06d59dc5
MihaiGaidau <gaidaumihai@gmai.com>
2020-02-28 17:30:56 +0200

    added security

--- firstExample/src/main/java/com/learnSOAP/practice/firstExample/configurations/WebServiceConfig.java
+++ firstExample/src/main/java/com/learnSOAP/practice/firstExample/configurations/WebServiceConfig.java
@@ -7,14 +7,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.support.KeyStoreFactoryBean;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.KeyStoreCallbackHandler;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
+import org.springframework.ws.soap.security.xwss.callback.SpringCertificateValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import javax.security.auth.callback.CallbackHandler;
+import java.security.KeyStore;
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 @EnableWs
-public class WebServiceConfig {
+public class WebServiceConfig extends WsConfigurerAdapter {
 
     @Bean
     public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context){
@@ -40,6 +52,83 @@ public class WebServiceConfig {
         return  definition;
     }
 
+    @Bean
+    public XwsSecurityInterceptor securityInterceptor(){
+        XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+        securityInterceptor.setCallbackHandler(callbackHandler());
+        securityInterceptor.setPolicyConfiguration(new ClassPathResource("/static/securityPolicy.xml"));
+        return  securityInterceptor;
+    }
+
+    @Bean
+    public SimplePasswordValidationCallbackHandler callbackHandler() {
+        SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+        handler.setUsersMap(Collections.singletonMap("user","password"));
+        return handler;
+    }
+// A password may be given to check the integrity of the keystore data.
+// If a password is not given, integrity checking is not performed.
+//    @Bean
+//    public KeyStoreFactoryBean keyStore(){
+//        KeyStoreFactoryBean keyStoreFactoryBean = new KeyStoreFactoryBean();
+//        keyStoreFactoryBean.setPassword("password");
+//        keyStoreFactoryBean.setLocation(new ClassPathResource("test-keystore.jks"));
+//        return keyStoreFactoryBean;
+//    }
+
+    //For instance, if you want to use the KeyStoreCallbackHandler to validate incoming
+    // certificates or signatures, you would use a trust store, like so:
+//    @Bean
+//    public KeyStoreCallbackHandler keyStoreHandler(){
+//        KeyStoreCallbackHandler handler = new KeyStoreCallbackHandler();
+//        handler.setTrustStore(trustStore().getObject());
+//        return handler;
+//    }
+//
+//    @Bean
+//    public KeyStoreFactoryBean trustStore(){
+//        KeyStoreFactoryBean keyStoreFactoryBean = new KeyStoreFactoryBean();
+//        keyStoreFactoryBean.setLocation(new ClassPathResource("truststore.jks"));
+//        keyStoreFactoryBean.setPassword("password");
+//        return trustStore();
+//    }
+
+
+//    @Bean
+//    public SpringCertificateValidationCallbackHandler certificateHandlerHandler(){
+//        SpringCertificateValidationCallbackHandler handler = new SpringCertificateValidationCallbackHandler();
+//        return handler;
+//    }
+//
+//    @Bean
+//    public XwsSecurityInterceptor securityInterceptorCertificate(){
+//        XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+//        securityInterceptor.setCallbackHandler(certificateHandlerHandler());
+//        return securityInterceptor;
+//    }
+
+    //decrypt incoming certificates or sign outgoing messages, you would use a key store
+    @Bean
+    public KeyStoreCallbackHandler keyStoreHandler(){
+        KeyStoreCallbackHandler handler = new KeyStoreCallbackHandler();
+        handler.setKeyStore(keyStore().getObject());
+        handler.setPrivateKeyPassword("changeIt");
+        return handler;
+
+    }
+
+    @Bean
+    public KeyStoreFactoryBean keyStore() {
+        KeyStoreFactoryBean keyStoreFactoryBean = new KeyStoreFactoryBean();
+        keyStoreFactoryBean.setLocation(new ClassPathResource("keystore.jks"));
+        keyStoreFactoryBean.setPassword("changeIt");
+        return keyStoreFactoryBean;
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
+    }
 
 
 }
